12d0e91d17c0062e4083ed0a966832e3364b69f0
MatheusTHM <thommesani@gmail.com>
2021-01-19 19:42:02 -0300

    Inicio do Curso CRUD com Laravel do Webdesign em foco

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+@extends('templates.template')
+
+@section('content')
+    <H1 class="text-center">CRUD</H1>
+    <hr>
+
+    <div class="text-center mt-3 mb-4">
+        <a href="">
+            <button class="btn btn-success">Cadastrar</button>
+        </a>
+    </div>
+    <div class="col-8 m-auto">
+        <table class="table table-hover table-dark text-center">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Título</th>
+                <th scope="col">Autor</th>
+                <th scope="col">Preco</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach($book as $books)
+                    @php
+                        $user=$books->find($books->id)->relUsers;
+                    @endphp
+                    <tr>
+                        <th scope="row">{{$books->id}}</th>
+                        <td>{{$books->title}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$books->price}}</td>
+                        <td>
+                            <a href="{{url("books/$books->id")}}">
+                                <button class="btn btn-light">Visualizar</button>
+                            </a>
+
+                            <a href="">
+                                <button class="btn btn-primary">Editar</button>
+                            </a>
+
+                            <a href="">
+                                <button class="btn btn-danger">Deletar</button>
+                            </a>
+                        </td>
+                      </tr>
+                @endforeach
+              
+            </tbody>
+          </table>
+    </div>
+@endsection
---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>CRUD com Laravel</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
